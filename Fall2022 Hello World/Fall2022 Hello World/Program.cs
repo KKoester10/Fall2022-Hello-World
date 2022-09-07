58f9ef517780f9a97fcfaaa1949ce3b32bf5bbf8
Kolton Koester <koltonkoester10@gmail.com>
2022-09-07 15:14:59 -0400

    learned Variable and Operators

--- Fall2022 Hello World/Fall2022 Hello World/Program.cs
+++ Fall2022 Hello World/Fall2022 Hello World/Program.cs
@@ -4,11 +4,62 @@
 
 //Hello World Workshop
 Console.WriteLine("Hello, World!");
+// Variables and Objects
+
+// Variable Declaration
+// Variable Declaration is stating that we have a variable and giving it an associated name 
+// if you are just doing variable declaration, you must state the variable type first
+
+// variableType variableName;
+    String Instructor;
+
+// Variable Assignment
+// Variable assignment is giving a variable a value (of the appropriate type) for us to reference later
+
+//variableName = value;
+    Instructor = "Gavin";
+// Variable Reassignment
+    Instructor = "Davis";
+
+// All together now!
+// if we use 'var', C# will understand that age is and integer based on the value from the right of the equal sign.
+
+// var/variableType variableName = value; 
+    var age = 24;
+    Console.WriteLine(age);
+
+// Operators 
+// Performs some kind of operation/action on some values
+//examples 5 + 13 -> 18
+
+// Assignment Operator
+// Single equal sign
+// Use this to assign the value of a variable
+
+// Arithmetic:
+// addition: +
+    var studentCount = 5 + 13;
+// subtraction: -
+// Multiplication: *
+// Divison: /
+    var averageClassSize = studentCount / 4f; // 18 / 3 -> 6 || 18 / 4 -> Not a Whole Number (4-5) ||18 / 4f -> 4.5f
+    Console.WriteLine(averageClassSize);
+// Exponentiation: ^
+// Modulo: %
+// -- Returns the remainder after dividing by a particular number
+    var time = 13 % 12; //time has a value of 1 here.
+// Equality
+
+// comparison
+
+// Incrementing 
+
+
 //white space does not matter.
 Console.WriteLine("Hello World , My name is Kolton" + " yes theses things matter");
-String characterName = "Asbar";
+String characterName = "Asbar ";
 int oneFive = 15;
 //will write characterName 15 times
 for (int i = 0; i <= oneFive; i++){
-   Console.WriteLine(characterName);
+   Console.WriteLine(characterName + i);
 }
