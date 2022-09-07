82646e74b17555790143f303f2812844d158ab85
Kolton Koester <koltonkoester10@gmail.com>
2022-09-07 11:30:18 -0400

    wrote out Hello World

---
+++ Fall2022 Hello World/Fall2022 Hello World/Program.cs
@@ -0,0 +1,14 @@
+﻿//what is this stuff?
+//what is a 'console' application?
+//what's with the fancy coloring? 
+
+//Hello World Workshop
+Console.WriteLine("Hello, World!");
+//white space does not matter.
+Console.WriteLine("Hello World , My name is Kolton" + " yes theses things matter");
+String characterName = "Asbar";
+int oneFive = 15;
+//will write characterName 15 times
+for (int i = 0; i <= oneFive; i++){
+   Console.WriteLine(characterName);
+}
